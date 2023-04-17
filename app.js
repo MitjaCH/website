22aa773a7e6bc8ff8a1348aff97cb5e2d67d4d29
MitjaCH <123660565+MitjaCH@users.noreply.github.com>
2023-04-17 10:56:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
+
+  function realtimeClock() {
+    
+    var rtClock = new Date();
+
+    var hours = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+    var seconds = rtClock.getSeconds();
+
+    var amPm = ( hours < 12 ) ? "AM" : "PM";
+
+    hours = (hours > 12) ? hours - 12 : hours;
+
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    document.getElementById('clock').innerHTML = 
+        hours + "  :  " + minutes + "  :  " + seconds + " " + amPm;
+    var t = setTimeout(realtimeClock, 500);
+
+}
